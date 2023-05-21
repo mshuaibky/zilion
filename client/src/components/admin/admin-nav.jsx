afbca605a74e8213b1ee238f6190a2ecc0ba2c8a
mshuhaibky <shuhaibkymohamed@gmail.com>
2023-05-21 17:01:16 +0530

    second commit

--- client/src/components/admin/admin-nav.jsx
+++ client/src/components/admin/admin-nav.jsx
@@ -47,62 +47,20 @@ function AdminNav() {
             />
           </svg>
         </a>
+        <h1 className='text-teal-600 text-lg'>Admin</h1>
       </div>
 
       <div className="hidden md:block">
         <nav aria-label="Site Nav">
           <ul className="flex items-center gap-6 text-sm">
-            <li>
-              <a
-                className="text-gray-500 transition hover:text-gray-500/75"
-                href="/"
-              >
-                About
-              </a>
-            </li>
+            
 
             <li>
               <a
                 className="text-gray-500 transition hover:text-gray-500/75"
                 href="/"
               >
-                Careers
-              </a>
-            </li>
-
-            <li>
-              <a
-                className="text-gray-500 transition hover:text-gray-500/75"
-                href="/"
-              >
-                History
-              </a>
-            </li>
-
-            <li>
-              <a
-                className="text-gray-500 transition hover:text-gray-500/75"
-                href="/"
-              >
-                Services
-              </a>
-            </li>
-
-            <li>
-              <a
-                className="text-gray-500 transition hover:text-gray-500/75"
-                href="/"
-              >
-                Projects
-              </a>
-            </li>
-
-            <li>
-              <a
-                className="text-gray-500 transition hover:text-gray-500/75"
-                href="/"
-              >
-                Blog
+                Home
               </a>
             </li>
           </ul>
